a72ba0a93154209a2d91f5996e6de4752d9a889b
Нарышкина Анна Андреевна <anyaivanova17092006@gmail.com>
2026-03-05 15:12:13 +0300

    Задание 2: Добавлена функция greet_user в main.py

--- src/main.py
+++ src/main.py
@@ -2,3 +2,16 @@
 
 print("Welcome to Git Learning Project!")
 print("This is version 1 of the application.")
+
+# Version 2 feature: User greeting
+name = "Student"
+print(f"Hello, {name}!")
+print("Enjoying the Git learning experience?")
+
+def greet_user(name):
+    # Function to greet users personalized.
+    return f"Welcome, {name}! Ready to learn Git?"
+
+if __name__ == "__main__":
+    result = greet_user("Student")
+    print(result)
